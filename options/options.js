aaff92699b1d0ecf4bb590d094bf8b4aa36febab
Crowdbotics <team@crowdbotics.com>
2023-12-20 10:31:10 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lively-water-45133",
+  url: "https://lively-water-45133.botics.co",
+  api: "https://lively-water-45133.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
